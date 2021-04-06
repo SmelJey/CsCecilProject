d4241a1a029b8334f28418bd2bf5e8fce4dae58d
SmelJey <mixalevuriy@gmail.com>
2021-04-06 18:20:03 +1000

    Add decimal add / sub support

--- CsCecilProject/Program.cs
+++ CsCecilProject/Program.cs
@@ -13,34 +13,55 @@ namespace CsCecilProject {
             string outputPath = args[1];
 
             var assembly = AssemblyDefinition.ReadAssembly(inputPath);
-            Console.WriteLine(assembly.FullName);
+            Console.WriteLine($"Input assembly: {assembly.FullName}");
+            int replacementsCnt = 0;
 
             foreach (ModuleDefinition module in assembly.Modules) {
                 foreach (TypeDefinition type in module.Types) {
                     foreach (MethodDefinition method in type.Methods) {
-                        ProcessMethod(method);
+                        replacementsCnt += ProcessMethod(method, module);
                     }
                 }
             }
 
             assembly.Write(outputPath);
+            Console.WriteLine($"{replacementsCnt} replacements done");
+            Console.WriteLine($"Output assembly was written to {outputPath}");
         }
 
-        private static void ProcessMethod(MethodDefinition method) {
+        private static int ProcessMethod(MethodDefinition method, ModuleDefinition module) {
             ILProcessor ilProcessor = method.Body.GetILProcessor();
+            int replacementsCnt = 0;
+
             for (int i = 0; i < method.Body.Instructions.Count; i++) {
                 var instruction = method.Body.Instructions[i];
-                if (instruction.OpCode == OpCodes.Add) {
-                    ilProcessor.Replace(instruction, Instruction.Create(OpCodes.Sub));
-                } else if (instruction.OpCode == OpCodes.Add_Ovf) {
-                    ilProcessor.Replace(instruction, Instruction.Create(OpCodes.Sub_Ovf));
-                } else if (instruction.OpCode == OpCodes.Add_Ovf_Un) {
-                    ilProcessor.Replace(instruction, Instruction.Create(OpCodes.Sub_Ovf_Un));
-                } else if (instruction.OpCode == OpCodes.Call && instruction.Operand is MethodReference methodReference) {
-                    // TODO: decimal add
+                Instruction replacementInstruction = null;
 
+                if (instruction.OpCode == OpCodes.Add) {
+                    replacementInstruction = Instruction.Create(OpCodes.Sub);
+
+                } else if (instruction.OpCode == OpCodes.Add_Ovf) {
+                    replacementInstruction = Instruction.Create(OpCodes.Sub_Ovf);
+
+                } else if (instruction.OpCode == OpCodes.Add_Ovf_Un) {
+                    replacementInstruction = Instruction.Create(OpCodes.Sub_Ovf_Un);
+
+                } else if (instruction.OpCode == OpCodes.Call && instruction.Operand is MethodReference methodReference) {
+                    var decimalAdd = module.ImportReference(typeof(decimal).GetMethod("op_Addition"));
+                    var decimalSub = module.ImportReference(typeof(decimal).GetMethod("op_Subtraction"));
+
+                    if (methodReference.FullName == decimalAdd.FullName) {
+                        replacementInstruction = Instruction.Create(OpCodes.Call, decimalSub);
+                    }
+                }
+
+                if (replacementInstruction != null) {
+                    ilProcessor.Replace(instruction, replacementInstruction);
+                    replacementsCnt++;
                 }
             }
+
+            return replacementsCnt;
         }
     }
 }
